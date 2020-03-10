0123c8b6018dc04b117ddb84a840108d91aa0093
Enrique Mora <enriquebeta7@gmail.com>
2020-03-10 13:42:42 -0300

    Componentizando o bloco countdown
      Quebrado em dois o componente Countdown para ter o contador e o title
      do contador separado em componentes.
    
    close #7

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -1,7 +1,6 @@
 // Dependencies
 import React, { useState } from 'react'
 import { useCssHandles } from 'vtex.css-handles'
-import { FormattedMessage } from 'react-intl'
 
 // Typings
 import { TimeSplit } from './typings/global'
@@ -10,36 +9,28 @@ import { TimeSplit } from './typings/global'
 import { tick } from './utils/time'
 
 interface CountdownProps {
-  title: string,
   targetDate: string
 }
 
 const DEFAULT_TARGET_DATE = (new Date('2020-03-11')).toISOString()
 
 // CSS HANDLES
-const CSS_HANDLES = ['countdown', 'container', 'title']
+const CSS_HANDLES = ['countdown']
 
-const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ title, targetDate }) => {
+const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ targetDate }) => {
   const [timeRemaining, setTime] = useState<TimeSplit>({
     hours: '00',
     minutes: '00',
     seconds: '00'
   })
 
-  const titleText = title || <FormattedMessage id="countdown.title"/>
   const handles = useCssHandles(CSS_HANDLES)
 
   tick(targetDate, setTime);
 
   return (
-    <div className={`${handles.container} t-heading-2 fw3 w-100 c-muted-1`}>
-      <div className={`${handles.title} db tc`}>
-        { titleText }
-      </div>
-
-      <h1 className={`${handles.countdown} db tc`}>
-        {`${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}`}
-      </h1>
+    <div className={`${handles.countdown} db tc`}>
+      {`${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}`}
     </div>
   )
 }
@@ -49,11 +40,6 @@ Countdown.schema = {
   description: 'editor.countdown.description',
   type: 'object',
   properties: {
-    title: {
-      title: 'Sou um título',
-      type: 'string',
-      default: null,
-    },
     targetDate: {
       title: 'Sou um título',
       description: 'Sou uma descrição',
