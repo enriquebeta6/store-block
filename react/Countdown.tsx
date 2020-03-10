986dd270707e93a71cd803550f32204b45c69b82
Enrique Mora <enriquebeta7@gmail.com>
2020-03-10 11:32:40 -0300

    Adicionada a funcionalidade do bloco countdown
    Implementado a funcionalidade de contador.
    
    close #4

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -1,12 +1,31 @@
-import React, { Fragment } from 'react'
+// Dependencies
+import React, { Fragment, useState } from 'react'
+
+// Typings
+import { TimeSplit } from './typings/global'
+
+// Utils
+import { tick } from './utils/time'
 
 interface CountdownProps {
   targetDate: string
 }
 
+const DEFAULT_TARGET_DATE = (new Date('2020-03-11')).toISOString()
+
 const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ targetDate }) => {
+  const [timeRemaining, setTime] = useState<TimeSplit>({
+    hours: '00',
+    minutes: '00',
+    seconds: '00'
+  });
+
+  tick(targetDate, setTime);
+
   return (<Fragment>
-    <h1>{ targetDate }</h1>
+    <h1>
+      {`${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}`}
+    </h1>
   </Fragment>)
 }
 
@@ -19,7 +38,7 @@ Countdown.schema = {
       title: 'Sou um título',
       description: 'Sou uma descrição',
       type: 'string',
-      default: null
+      default: DEFAULT_TARGET_DATE
     }
   },
 }
